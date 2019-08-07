8b11b12958a08bb437bbd76a05371cfe4d40afad
Cairo Ramos <cairo.ramosoliveira4@gmail.com>
2019-08-07 07:38:40 -0300

    Implementations of UI and Redux

---
+++ src/api.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyAtvQ1X3qcKbFH0MCdrginHdaeyk066Iz0'
+
+export default API_KEY
---
+++ src/components/SearchBar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+import { Segment, Input } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { buscaVideo } from './../store/actions/busca-video'
+
+class SearchBar extends Component {
+    pesquisaTermo = e => {
+        if (e.keyCode === 13) {
+            const termo = e.target.value
+            this.props.handleBuscaVideo(termo)
+        }
+    }
+
+    render() {
+        return (
+            <div className='search-bar'>
+                <Segment stacked>
+                    <Input
+                        icon='search'
+                        onKeyDown={e => this.pesquisaTermo(e)}
+                        size='large'
+                        placeholder='Search...'
+                    />
+                </Segment>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        handleBuscaVideo: termo => dispatch(buscaVideo(termo))
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(SearchBar)
